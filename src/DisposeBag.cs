fcb3f74206d5dd8b05ee2a73f1b2976e53165a60
Joe Gallagher <joegaggler@gmail.com>
2024-11-15 09:25:55 -0500

    check for null disposable in dispose bag

--- src/DisposeBag.cs
+++ src/DisposeBag.cs
@@ -7,11 +7,16 @@ public class DisposeBag : IDisposable
 {
     private readonly Stack<IDisposable> items = new();
 
-    public void Add(IDisposable disposable) => items.Push(disposable);
+    public void Add(IDisposable? disposable)
+    {
+        if (disposable is null) { return; }
+        items.Push(disposable);
+    }
+
 
     public void Dispose()
     {
-        while (items.TryPop(out IDisposable? item))
+        while (items.TryPop(out var item))
         {
             item.Dispose();
         }
